b2d40c8b7adc28c84a45eec3ec2263c645d60d69
Olusegun <shegoj@yahoo.com>
2024-10-27 18:48:50 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# check101
+# check101
---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0acc77abdfc7ed5a6"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
